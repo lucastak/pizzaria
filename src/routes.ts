c57ca3cf85b505b2f33850a0a703a5226ed588e6
lucastak <lucas.santos@hotmail.com>
2022-09-17 18:26:19 -0300

    feat(project): adiciona feature de criar categoria

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuhenticated } from "./middlewares/isAuthenticated";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 const router = Router();
 
@@ -12,5 +13,7 @@ router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
 
 router.get("/me", isAuhenticated, new DetailUserController().handle);
+
+router.post("/category", isAuhenticated, new CreateCategoryController().handle);
 
 export { router };
